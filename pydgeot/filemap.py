dd11f6a9d4c93e4106cc217bbda898222a1e2cef
Derrick Staples <broiledmeat@gmail.com>
2013-01-08 20:04:59 -0500

    Rename sourcesstore to filemap. Sources request can now take a root path, returning only sources directly under that path.

--- pydgeot/filemap.py
+++ pydgeot/filemap.py
@@ -1,12 +1,18 @@
 import os
 import sqlite3
-from . import abspath
+
+def re_fn(expr, item):
+    import re
+    reg = re.compile(expr, re.I)
+    return reg.search(item) is not None
 
 class FileMap:
-    def __init__(self, core, path):
-        self.core = core
+    def __init__(self, app, path):
+        self.app = app
         self.connection = sqlite3.connect(path)
         self.cursor = self.connection.cursor()
+
+        self.connection.create_function('REGEXP', 2, re_fn)
 
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS sources (
@@ -41,13 +47,30 @@ class FileMap:
     def commit(self):
         self.connection.commit()
 
-    def sources(self):
-        results = self.cursor.execute('SELECT path FROM sources')
-        for result in results:
-            yield self.source(result[0])
+    def wipe(self):
+        self.cursor.execute("DELETE FROM source_dependencies")
+        self.cursor.execute("DELETE FROM source_targets")
+        self.cursor.execute("DELETE FROM sources")
+        self.commit()
+
+    def sources(self, prefix=None, mtimes=False):
+        if prefix is None:
+            results = self.cursor.execute('SELECT path, modified FROM sources')
+        else:
+            rel = self._relative(prefix)
+            if rel == '':
+                regex = '^([^{0}]*)$'.format(os.sep)
+            else:
+                regex = '^{0}{1}([^{1}]*)$'.format(rel, os.sep)
+            regex = regex.replace('\\', '\\\\')
+            results = self.cursor.execute('SELECT path, modified FROM sources WHERE path REGEXP ?', (regex, ))
+        if mtimes:
+            return [(self._source(result[0]), result[1]) for result in results]
+        else:
+            return [self._source(result[0]) for result in results]
 
     def remove(self, source):
-        rel = self.relative(source)
+        rel = self._relative(source)
         self.cursor.execute("""
             DELETE
             FROM sources
@@ -55,7 +78,7 @@ class FileMap:
             """, (rel, ))
 
     def targets(self, source, values=None):
-        rel = self.relative(source)
+        rel = self._relative(source)
         if values is not None:
             self.cursor.execute("""
                 DELETE
@@ -71,17 +94,17 @@ class FileMap:
                 INSERT INTO source_targets
                     (source_id, path)
                     VALUES (?, ?)
-                """, ([(id, value) for value in values]))
+                """, ([(id, self._relative(value)) for value in values]))
         results = self.cursor.execute("""
             SELECT st.path
             FROM source_targets AS st
                 INNER JOIN sources s ON s.id = st.source_id
             WHERE s.path = ?
             """, (rel, ))
-        return [self.source(result[0]) for result in results]
+        return [self._source(result[0]) for result in results]
 
     def dependencies(self, source, values=None):
-        rel = self.relative(source)
+        rel = self._relative(source)
         if values is not None:
             self.cursor.execute("""
                 DELETE
@@ -107,10 +130,10 @@ class FileMap:
                 INNER JOIN sources d ON d.id = sd.dependency_id
             WHERE s.path = ?
             """, (rel, ))
-        return [self.source(result[0]) for result in results]
+        return [self._target(result[0]) for result in results]
 
     def _add_source(self, source):
-        rel = self.relative(source)
+        rel = self._relative(source)
         try:
             stats = os.stat(source)
             size = stats.st_size
@@ -125,8 +148,16 @@ class FileMap:
                 """, (rel, size, mtime))
         return self.cursor.lastrowid
 
-    def source(self, relative):
-        return abspath(os.path.join(self.core.source_root, relative))
+    def _source(self, relative):
+        return os.path.join(self.app.content_root, relative)
 
-    def relative(self, source):
-        return os.path.relpath(source, self.core.source_root)
+    def _target(self, relative):
+        return os.path.join(self.app.build_root, relative)
+
+    def _relative(self, path):
+        if path.startswith(self.app.content_root):
+            path = os.path.relpath(path, self.app.content_root)
+        elif path.startswith(self.app.build_root):
+            path = os.path.relpath(path, self.app.build_root)
+        path = '' if path == '.' else path
+        return path

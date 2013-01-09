696c8623b8e17b0186702ba823aeeb7783f1b140
Derrick Staples <broiledmeat@gmail.com>
2013-01-09 09:32:30 -0500

    Unstub Jinja processor. Add processor and dependencies getter. Don't process files with 'template_only' variable.

--- pydgeot/processors/builtins/jinja.py
+++ pydgeot/processors/builtins/jinja.py
@@ -1,5 +1,52 @@
+import os
+import jinja2
 from pydgeot.processors import register, Processor
 
 @register()
 class JinjaProcessor(Processor):
-    pass
+    def __init__(self, app):
+        super().__init__(app)
+        self.envs = {}
+
+    def can_process(self, path):
+        return path.endswith('.html')
+
+    def process(self, path):
+        if not self._has_template_flag(path):
+            env = self._get_env(self.app.content_root)
+            content = open(path).read()
+            template = env.from_string(content)
+            rel = os.path.relpath(path, self.app.content_root)
+            target = os.path.join(self.app.build_root, rel)
+            f = open(target, 'w')
+            f.write(template.render())
+            f.close()
+            return [target]
+        return []
+
+    def get_dependencies(self, path):
+        body = self._get_env(self.app.content_root).parse(open(path).read()).body
+        return self._find_deps(self.app.content_root, body)
+
+    def _get_env(self, root):
+        if root not in self.envs:
+            self.envs[root] = jinja2.Environment(loader=jinja2.FileSystemLoader(root))
+        return self.envs[root]
+
+    def _has_template_flag(self, path):
+        body = self._get_env(self.app.content_root).parse(open(path).read()).body
+        for part in body:
+            if isinstance(part, jinja2.nodes.Assign) and \
+               part.target.name == 'template_only' and \
+               part.node.value:
+                return True
+        return False
+
+    def _find_deps(self, root, body):
+        deps = set()
+        for part in body:
+            if isinstance(part, (jinja2.nodes.Extends, jinja2.nodes.Include)):
+                deps.add(os.path.join(root, part.template.value))
+            elif isinstance(part, jinja2.nodes.Block):
+                deps |= self._find_deps(root, part.body)
+        return deps

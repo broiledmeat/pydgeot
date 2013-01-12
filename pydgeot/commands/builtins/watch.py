87734996c6d65649af6600348e3b9f05db96f668
Derrick Staples <broiledmeat@gmail.com>
2013-01-12 14:13:14 -0500

    Add continuous generator

---
+++ pydgeot/commands/builtins/watch.py
@@ -0,0 +1,22 @@
+def watch(app, *args):
+    import os
+    import types
+    from pydgeot.commands import CommandError
+    from pydgeot.generator import Generator
+    from pydgeot.fsobserver import FSObserver
+
+    if app.is_valid:
+        gen = Generator(app)
+        if len(args) > 0 and args[0] == 'wipe':
+            gen.wipe()
+        gen.generate()
+
+        obs = FSObserver(app.content_root)
+        def on_changed(self, path):
+            root = os.path.dirname(path)
+            changes = gen.collect_changes(root)
+            gen.process_changes(changes)
+        setattr(FSObserver, on_changed.__name__, types.MethodType(on_changed, FSObserver))
+        obs.start()
+    else:
+        raise CommandError('Need a valid Pydgeot app directory.')

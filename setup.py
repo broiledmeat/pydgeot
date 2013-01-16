c61f2f3ed89d1f1e815efbf9eeac129db49c8f03
Derrick Staples <broiledmeat@gmail.com>
2013-01-15 20:28:26 -0500

    Add setup and license

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='pydgeot',
+    version='0.2',
+    packages=['pydgeot', 'pydgeot.processors', 'pydgeot.commands', 'pydgeot.commands.builtins'],
+    scripts=['scripts/pydgeot'],
+    url='https://github.com/broiledmeat/pydgeot',
+    license='Apache License, Version 2.0',
+    author='Derrick Staples',
+    author_email='broiledmeat@gmail.com',
+    description='Static content generator'
+)

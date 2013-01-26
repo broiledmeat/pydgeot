34dd0c33ebf6ece1876522a836b3630cf53e5c56
Derrick Staples <broiledmeat@gmail.com>
2013-01-26 14:37:33 -0500

    Initial documentation for the filemap module.
    This reveals some naming inconsistencies and some methods that could
     possibly use some refactoring. These issues will be addressed later.

--- pydgeot/filemap.py
+++ pydgeot/filemap.py
@@ -2,12 +2,29 @@ import os
 import sqlite3
 
 def re_fn(expr, item):
+    """
+    REGEXP function for SQLite. Return true if a match is found.
+    """
     import re
     reg = re.compile(expr, re.I)
     return reg.search(item) is not None
 
 class FileMap:
+    """
+    Stores file information for generated content in a persistent SQLite database. When content is being generated, this
+    information is used to determine what files are new, updated, or deleted. It also stores what files are built from
+    a source file, as well as what other source files it may depend on.
+
+    All methods expect absolute paths, in either the Apps content or build directories.
+    """
     def __init__(self, app, path):
+        """
+        Establish an SQLite database connection, and create tables if necessary.
+
+        Args:
+            app: Parent App instance.
+            path: SQLite database path.
+        """
         self.app = app
         self.connection = sqlite3.connect(path)
         self.cursor = self.connection.cursor()
@@ -45,9 +62,15 @@ class FileMap:
             )""")
 
     def commit(self):
+        """
+        Commit database changes.
+        """
         self.connection.commit()
 
     def reset(self):
+        """
+        Delete all entries.
+        """
         self.cursor.execute("DELETE FROM source_dependencies")
         self.cursor.execute("DELETE FROM source_targets")
         self.cursor.execute("DELETE FROM sources")
@@ -57,6 +80,12 @@ class FileMap:
         self.commit()
 
     def clean(self, paths):
+        """
+        Delete entries under the given source directories and their subdirectories.
+
+        Args:
+            paths: List of content directory paths to delete entries for.
+        """
         for path in paths:
             regex = self._regex(path, subdirs=True)
             self.cursor.execute("SELECT id FROM sources WHERE path REGEXP ?", (regex, ))
@@ -74,6 +103,17 @@ class FileMap:
         self.commit()
 
     def get_sources(self, prefix=None, mtimes=False):
+        """
+        Get a list of source paths.
+
+        Args:
+            prefix: Source directory to get files in. Not recursive.
+            mtimes: Return modified times for files as well.
+
+        Returns:
+            A list of source file paths. If mtimes is True, a list of tuples containing the source file path and its
+            modified time, will be returned.
+        """
         if prefix is None:
             results = self.cursor.execute('SELECT path, modified FROM sources')
         else:
@@ -85,6 +125,12 @@ class FileMap:
             return [self._source_path(result[0]) for result in results]
 
     def remove_source(self, source):
+        """
+        Remove a source entry, and any associated source dependencies and target files.
+
+        Args:
+            source: Source file path to remove.
+        """
         rel = self._relative_path(source)
         self.cursor.execute("SELECT id FROM sources WHERE path = ?", (rel, ))
         result = self.cursor.fetchone()
@@ -95,6 +141,17 @@ class FileMap:
             self.cursor.execute("DELETE FROM sources WHERE id = ?", (id, ))
 
     def get_targets(self, source, reverse=False):
+        """
+        Get a list of target paths that a source path has generated.
+
+        Args:
+            source: Source path to get targets path for.
+            reverse: Perform a reverse lookup instead. Returning source paths for a given target path. The source
+                     argument should be given a target path.
+
+        Returns:
+            A list of target paths (or source paths, if reverse is True.)
+        """
         rel = self._relative_path(source)
         if reverse:
             results = self.cursor.execute("""
@@ -114,6 +171,13 @@ class FileMap:
             return [self._target_path(result[0]) for result in results]
 
     def set_targets(self, source, values):
+        """
+        Set target paths for a source path.
+
+        Args:
+            source: Source path to set target paths for.
+            values: List of target paths.
+        """
         rel = self._relative_path(source)
         self.cursor.execute("""
             DELETE
@@ -132,6 +196,22 @@ class FileMap:
             """, ([(id, self._relative_path(value)) for value in values]))
 
     def get_dependencies(self, source, reverse=False):
+        """
+        Get a list of source paths that a source path depends on to generate.
+
+        If 'fileA.html' and 'fileB.html' are both templates that depend on 'base.html', then:
+         get_dependencies('fileA.html') => ['base.html']
+         get_dependencies('base.html') => []
+         get_dependencies('base.html', reverse=True) => ['fileA.html', 'fileB.html']
+
+        Args:
+            source: Source path to get dependency paths for.
+            reverse: Perform a reverse lookup instead. Return source paths that depend on the given source path to
+                     generate.
+
+        Returns:
+            A list of source paths.
+        """
         rel = self._relative_path(source)
         if reverse:
             results = self.cursor.execute("""
@@ -152,6 +232,13 @@ class FileMap:
         return [self._source_path(result[0]) for result in results]
 
     def set_dependencies(self, source, values):
+        """
+        Set source dependencies for a source path.
+
+        Args:
+            source: Source path to set dependency paths for.
+            values: List of source dependency paths.
+        """
         rel = self._relative_path(source)
         self.cursor.execute("""
             DELETE
@@ -172,6 +259,15 @@ class FileMap:
             """, [(id, value_id) for value_id in value_ids])
 
     def _add_source(self, source):
+        """
+        Add a source entry to the database. Updates file information if the entry already exists.
+
+        Args:
+            source: Source path to add.
+
+        Returns:
+            The entries database id.
+        """
         rel = self._relative_path(source)
         try:
             stats = os.stat(source)
@@ -196,12 +292,39 @@ class FileMap:
         return self.cursor.lastrowid
 
     def _source_path(self, relative):
+        """
+        Get a source path given a relative path.
+
+        Args:
+            relative: Relative path.
+
+        Returns:
+            A source path.
+        """
         return os.path.join(self.app.content_root, relative)
 
     def _target_path(self, relative):
+        """
+        Get a target path given a relative path.
+
+        Args:
+            relative: Relative path.
+
+        Returns:
+            A target path.
+        """
         return os.path.join(self.app.build_root, relative)
 
     def _relative_path(self, path):
+        """
+        Get a relative path from a source or target path.
+
+        Args:
+            path: Source or target path.
+
+        Returns:
+            A relative path.
+        """
         if path.startswith(self.app.content_root):
             path = os.path.relpath(path, self.app.content_root)
         elif path.startswith(self.app.build_root):
@@ -210,6 +333,16 @@ class FileMap:
         return path
 
     def _regex(self, path, subdirs=False):
+        """
+        Get a regex for the given directory path. Used for retrieving file paths in or under the given directory.
+
+        Args:
+            path: Directory path.
+            subdirs: Retrieve files in all subdirectories.
+
+        Returns:
+            A regex string.
+        """
         rel = self._relative_path(path)
         if subdirs:
             match = '.*'

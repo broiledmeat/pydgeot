be324c56151758d54e6c6f4b58f7657578b19e4d
Derrick Staples <broiledmeat@gmail.com>
2013-01-28 01:08:53 -0500

    Add docopt requirement to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from distutils.core import setup
 setup(
     name='pydgeot',
     version='0.2',
-    packages=['pydgeot', 'pydgeot.processors', 'pydgeot.processors.builtins', 'pydgeot.commands', 'pydgeot.commands.builtins'],
+    packages=[
+        'pydgeot',
+        'pydgeot.processors',
+        'pydgeot.processors.builtins',
+        'pydgeot.commands',
+        'pydgeot.commands.builtins'],
     scripts=['scripts/pydgeot'],
+    requires=['docopt'],
     url='https://github.com/broiledmeat/pydgeot',
     license='Apache License, Version 2.0',
     author='Derrick Staples',
     author_email='broiledmeat@gmail.com',
-    description='Static content generator'
+    description='Static content generator',
 )

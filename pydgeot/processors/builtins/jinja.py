24d74e7181ba85436585627a4e5bfe9592b0568a
derrick <broiledmeat@gmail.com>
2013-04-15 10:35:13 -0400

    Write Jinja templates as UTF-8. Add beginnings of support for context variables for Jinja templates.

--- pydgeot/processors/builtins/jinja.py
+++ pydgeot/processors/builtins/jinja.py
@@ -4,6 +4,11 @@ from pydgeot.processors import register, Processor
 
 @register()
 class JinjaProcessor(Processor):
+    TEMPLATE_FIELDS = {
+        'name': (str, None),
+        'template_only': (bool, False)
+    }
+
     def __init__(self, app):
         super().__init__(app)
         self.envs = {}
@@ -12,14 +17,15 @@ class JinjaProcessor(Processor):
         return path.endswith('.html')
 
     def process_update(self, path):
-        if not self._has_template_flag(path):
+        fields = self._get_template_fields(path)
+        if 'template_only' in fields and fields['template_only'] != 'true':
             env = self._get_env(self.app.source_root)
             content = open(path).read()
             template = env.from_string(content)
             rel = os.path.relpath(path, self.app.source_root)
             target = os.path.join(self.app.build_root, rel)
             os.makedirs(os.path.dirname(target), exist_ok=True)
-            f = open(target, 'w')
+            f = open(target, 'w', encoding='utf-8')
             f.write(template.render())
             f.close()
             return [target]
@@ -34,14 +40,24 @@ class JinjaProcessor(Processor):
             self.envs[root] = jinja2.Environment(loader=jinja2.FileSystemLoader(root))
         return self.envs[root]
 
-    def _has_template_flag(self, path):
+    def _get_template_fields(self, path):
+        fields = dict((name, val[1]) for name, val in self.TEMPLATE_FIELDS.items())
         body = self._get_env(self.app.source_root).parse(open(path).read()).body
-        for part in body:
-            if isinstance(part, jinja2.nodes.Assign) and \
-               part.target.name == 'template_only' and \
-               part.node.value:
-                return True
-        return False
+        parts = [(p.target.name.lower(), p.node) for p in body
+                 if isinstance(p, jinja2.nodes.Assign) and p.target.name.lower() in fields]
+        for name, node in parts:
+            fields[name] = self._get_template_field_value(node, self.TEMPLATE_FIELDS[name][0])
+        return fields
+
+    def _get_template_field_value(self, node, type=str):
+        if isinstance(node, jinja2.nodes.List):
+            values = dict(node.iter_fields())['items']
+            value = [self._get_template_field_value(v, type) for v in values]
+        elif type is bool:
+            value = isinstance(node.value, bool) and node.value
+        else:
+            value = str(node.value)
+        return value
 
     def _find_deps(self, root, body):
         deps = set()

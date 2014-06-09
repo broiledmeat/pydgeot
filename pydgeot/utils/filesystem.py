76f58dc648ab1af0aea0b78348eb426a6954441f
derrick <broiledmeat@gmail.com>
2014-06-09 10:05:08 -0400

    Add docstrings to the filesystem module.

--- pydgeot/utils/filesystem.py
+++ pydgeot/utils/filesystem.py
@@ -4,6 +4,14 @@ import stat
 
 
 def is_dotfile(path):
+    """
+    Check if any part of a file path is a dot file.
+
+    :param path: File path to check.
+    :type path: str
+    :return: If any part of the file path is a dot file.
+    :rtype: bool
+    """
     parts = path.split(os.sep)
     return any([part != '..' and part.startswith('.') for part in parts])
 
@@ -12,11 +20,27 @@ if sys.platform == 'win32':
         import win32file
 
         def create_symlink(source, target):
+            """
+            Create a symlink from target to source.
+
+            :param source: Source path for the symlink.
+            :type source: str
+            :param target: New path to create as the symlink.
+            :type target: str
+            """
             if os.path.isfile(target):
                 os.remove(target)
             win32file.CreateSymbolicLink(target, source)
 
         def is_hidden(path):
+            """
+            Check if the file path is hidden.
+
+            :param path: File path to check.
+            :type path: str
+            :return: If the file path is hidden.
+            :rtype: bool
+            """
             try:
                 return bool(win32file.GetFileAttributes(path) & 2)
             except (AttributeError, AssertionError):
@@ -27,10 +51,26 @@ if sys.platform == 'win32':
 
 elif sys.platform == 'darwin':
     def is_hidden(path):
+        """
+        Check if the file path is hidden.
+
+        :param path: File path to check.
+        :type path: str
+        :return: If the file path is hidden.
+        :rtype: bool
+        """
         return is_dotfile(path) or os.stat(path).st_flags & stat.UF_HIDDEN > 0
 
 if 'create_symlink' not in globals():
     def create_symlink(source, target):
+        """
+        Create a symlink from target to source.
+
+        :param source: Source path for the symlink.
+        :type source: str
+        :param target: New path to create as the symlink.
+        :type target: str
+        """
         if os.path.isfile(target):
             os.remove(target)
         os.symlink(source, target)

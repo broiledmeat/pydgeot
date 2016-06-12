fbc6d57c1167f6ab85de85bb836f7ed5263fff96
Derrick Staples <broiledmeat@gmail.com>
2016-06-12 12:23:52 -0400

    Catch import exceptions when loading the Jinja and LessCSS builtins.

--- pydgeot/processors/builtins/lesscss.py
+++ pydgeot/processors/builtins/lesscss.py
@@ -1,35 +1,40 @@
 import os
-from lesscpy.lessc import parser, formatter
 from pydgeot.processors import register, Processor
 
 
-@register()
-class LessCSSProcessor(Processor):
-    """
-    Compile a Less (http://lesscss.org/) source file in to the build directory.
-    """
-    name = 'LessCSS'
+try:
+    # noinspection PyUnresolvedReferences
+    from lesscpy.lessc import parser, formatter
+except ImportError:
+    pass
+else:
+    @register()
+    class LessCSSProcessor(Processor):
+        """
+        Compile a Less (http://lesscss.org/) source file in to the build directory.
+        """
+        name = 'LessCSS'
 
-    def __init__(self, app):
-        super().__init__(app)
-        self.parser = parser.LessParser()
-        self.formatter = formatter.Formatter(self._LessOpts())
+        def __init__(self, app):
+            super().__init__(app)
+            self.parser = parser.LessParser()
+            self.formatter = formatter.Formatter(self._LessOpts())
 
-    def can_process(self, path):
-        return path.endswith('.css')
+        def can_process(self, path):
+            return path.endswith('.css')
 
-    def generate(self, path):
-        self.parser.parse(filename=path)
-        rel = os.path.relpath(path, self.app.source_root)
-        target = os.path.join(self.app.build_root, rel)
-        os.makedirs(os.path.dirname(target), exist_ok=True)
-        with open(target, 'w') as fh:
-            fh.write(self.formatter.format(self.parser))
-        self.app.sources.set_targets(path, [target])
+        def generate(self, path):
+            self.parser.parse(filename=path)
+            rel = os.path.relpath(path, self.app.source_root)
+            target = os.path.join(self.app.build_root, rel)
+            os.makedirs(os.path.dirname(target), exist_ok=True)
+            with open(target, 'w') as fh:
+                fh.write(self.formatter.format(self.parser))
+            self.app.sources.set_targets(path, [target])
 
-    class _LessOpts:
-        def __init__(self):
-            self.minify = False
-            self.xminify = False
-            self.tabs = False
-            self.spaces = True
+        class _LessOpts:
+            def __init__(self):
+                self.minify = False
+                self.xminify = False
+                self.tabs = False
+                self.spaces = True

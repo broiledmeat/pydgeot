c8ad3c7e27dd2efde71944893d5c6d2219842437
Derrick Staples <broiledmeat@gmail.com>
2016-08-29 01:21:41 -0400

    Replace hacky pkgutil builtin command and processor importing with explicit imports.

--- pydgeot/processors/builtins/__init__.py
+++ pydgeot/processors/builtins/__init__.py
@@ -1,8 +1,4 @@
-import pkgutil
-
-
-# Import all modules in this package at runtime so processors are registered.
-__all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    loader.find_module(module_name).load_module(module_name)
+from .copyfallback import CopyFallbackProcessor
+from .symlinkfallback import SymlinkFallbackProcessor
+from .jinja import JinjaProcessor
+from .lesscss import LessCSSProcessor

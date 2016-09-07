7cf191df4df6fd0edf7877324e898f0d932af1b9
Derrick Staples <broiledmeat@gmail.com>
2016-09-07 00:26:27 -0400

    Remove the dependency DOT graph built-in (moving to a plugin.)

--- pydgeot/commands/builtins/__init__.py
+++ pydgeot/commands/builtins/__init__.py
@@ -1,7 +1,6 @@
 from .build import build
 from .clean import clean
 from .create import create
-from .depdot import depdot
 from .list_commands import list_commands
 from .list_plugins import list_plugins
 from .list_processors import list_processors

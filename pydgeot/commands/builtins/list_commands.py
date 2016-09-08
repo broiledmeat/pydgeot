f7b9578b2aa747443abac4e9b47a42d6d7ee5b46
Derrick Staples <broiledmeat@gmail.com>
2016-09-08 09:16:39 -0400

    Support appless running in the list commands command, and improve output formatting.

--- pydgeot/commands/builtins/list_commands.py
+++ pydgeot/commands/builtins/list_commands.py
@@ -1,7 +1,7 @@
 from pydgeot.commands import register
 
 
-@register(name='commands', help_msg='List available commands')
+@register(name='commands', help_msg='List available commands', allow_appless=True)
 def list_commands(app):
     """
     Print available commands information.
@@ -9,15 +9,19 @@ def list_commands(app):
     :param app: App instance to get commands for.
     :type app: pydgeot.app.App | None
     """
-    commands = sorted(app.commands.values(), key=lambda x: x.name)
+    from pydgeot import commands
+
+    commands = sorted(commands.available.values(), key=lambda x: x.name)
 
     if len(commands) == 0:
         return
 
-    left_align = max(14, max([len(c.name) + len(c.help_args) for c in commands])) + 4
+    name_align = max(14, max([len(command.name) for command in commands]))
+    args_align = max([len(command.help_args) for command in commands])
 
     for command in commands:
-        disp = command.name
-        if command.help_args != '':
-            disp += ' ' + command.help_args
-        print('{0}    {1}'.format(disp.rjust(left_align), command.help_msg))
+        if app is None and not command.allow_appless:
+            continue
+
+        print('{} {}    {}'.format(command.name.rjust(name_align), command.help_args.ljust(args_align),
+                                   command.help_msg))

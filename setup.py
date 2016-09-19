5922063949b688803e3937eb8be84f93870ea68b
Derrick Staples <broiledmeat@gmail.com>
2016-09-19 09:44:26 -0400

    Update setup and add manifest

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
+import setuptools
 from distutils.core import setup
+
+base_package = 'pydgeot'
+
+version = __import__('pydgeot').__version__
+packages = [base_package] + ['{}.{}'.format(base_package, package)
+                             for package in setuptools.find_packages(base_package)]
 
 setup(
     name='pydgeot',
-    version='0.4',
-    packages=[
-        'pydgeot',
-        'pydgeot.app',
-        'pydgeot.filesystem',
-        'pydgeot.commands',
-        'pydgeot.commands.builtins',
-        'pydgeot.processors',
-        'pydgeot.processors.builtins'],
-    scripts=['scripts/pydgeot'],
-    requires=['docopt'],
+    description='Plugin based static content generator',
     url='https://github.com/broiledmeat/pydgeot',
     license='Apache License, Version 2.0',
     author='Derrick Staples',
     author_email='broiledmeat@gmail.com',
-    description='Static content generator'
+    version=version,
+    packages=packages,
+    scripts=['scripts/pydgeot'],
+    requires=['docopt'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
